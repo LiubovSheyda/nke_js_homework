f6075115a2deceeeea5623043f79b3f3e8b09972
LiubovSheyda <liubovsheyda@gmail.com>
2020-03-23 13:00:51 +0700

    Update lesson_26.js

--- regular_expressions/lesson_26.js
+++ regular_expressions/lesson_26.js
@@ -2,6 +2,11 @@ console.log('|');
 console.log('| EXECUTION FROM TASK NUMBER 26');
 console.log('|');
 
+/*
+Внимательно прочитай условие задачи
+Вывод в консоль в формате 'aaa [4] bbb [6] ccc [24] ddd'
+return "[" + (result * 2) + "]";
+*/
 console.log(
     'aaa [2] bbb [3] ccc [12] ddd'
         .replace(/[(\d+)]/g, result => {
