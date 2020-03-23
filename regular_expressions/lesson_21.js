403a2b4c7a3283e89d57e712f0ad85b4b7fc18d2
LiubovSheyda <liubovsheyda@gmail.com>
2020-03-23 12:51:56 +0700

    Update lesson_21.js

--- regular_expressions/lesson_21.js
+++ regular_expressions/lesson_21.js
@@ -2,4 +2,8 @@ console.log('|');
 console.log('| EXECUTION FROM TASK NUMBER 21');
 console.log('|');
 
+/* 
+Чтобы брались только двузначные числа
+используются спец. символы \b
+*/
 console.log('12 34 56 78'.replace(/(\d)(\d)/g, '$2$1'));
