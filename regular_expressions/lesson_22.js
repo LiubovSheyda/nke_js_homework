b92cf2bc38ebf7ed0b7e8a5caa79e6f510ca6496
LiubovSheyda <liubovsheyda@gmail.com>
2020-03-23 12:55:31 +0700

    Update lesson_22.js

--- regular_expressions/lesson_22.js
+++ regular_expressions/lesson_22.js
@@ -2,6 +2,10 @@ console.log('|');
 console.log('| EXECUTION FROM TASK NUMBER 22');
 console.log('|');
 
+/*
+.ru и .com нет универсальности
+по заданию протокол http
+*/
 console.log(
     'sss site.ru zzz site.com kkk'
         .replace(
